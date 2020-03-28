a4d3d342386402177a7017dc61c26f50af48c1c2
Drack95 <fo.vigouroux@laposte.net>
2020-03-28 19:52:41 +0100

    HTML

--- script/dice.js
+++ script/dice.js
@@ -5,7 +5,7 @@ on("chat:message", function(msg) {
             let dice = rollTorgDice().toString();
             let res = {
                 title:'Jet de dès',
-                infoList:['resultat: '+dice]
+                infoList:[["resultat",dice]]
             }
             sendResultToChat(msg.who, res);
         }
@@ -14,7 +14,7 @@ on("chat:message", function(msg) {
             let score = diceToScore(dice).toString();
             let res = {
                 title:'Jet de Score',
-                infoList:['resultat: '+score]
+                infoList:[["resultat",score]]
             }
             sendResultToChat(msg.who, res);
         }
@@ -142,12 +142,12 @@ function attackPlayer(attackerSkill, difficulty, playerName, weaponModifier, wea
     if (isSuccess(score, attackerSkill, difficulty)) {
         log('success attack');
         result.title = `${playerName} a été touché`;
-        result.infoList = ["Scoce obtenu: "+score];
+        result.infoList = [["Score obtenu",score]];
         result.infoList.push(...computeDommagePossibilite(weaponModifier, weaponMax, score, playerToughness));
     } else {
         log('failed attack');
         result.title = `${playerName} a esquivé l'attaque`;
-        result.infoList = ["Scoce obtenu: "+score];
+        result.infoList = [["Score obtenu",score]];
     }
     return result;
 }
@@ -163,108 +163,147 @@ function computeDommagePossibilite(weaponModifier, weaponMax, score, playerTough
 
 function margeToDamageForCharacterWithPossibility(marge) {
     if (marge <= 0) {
-        return ["Aucun dommage"];
+        return [["!","Aucun dommage"]];
     }
     if (marge <= 1) {
-        return ["Dommage : 1"];
+        return [["Dommage","1"]];
     }
     if (marge <= 2) {
-        return ["Dommage : O 1"];
+        return [["Dommage","O 1"]];
     }
     if (marge <= 3) {
-        return ["Dommage : K 1"];
+        return [["Dommage","K 1"]];
     }
     if (marge <= 4) {
-        return ["Dommage : 2"];
+        return [["Dommage","2"]];
     }
     if (marge <= 5) {
-        return ["Dommage : O 2"];
+        return [["Dommage","O 2"]];
     }
     if (marge <= 6) {
-        return ["Dommage : Chute O 2"];
+        return [["Dommage","Chute O 2"]];
     }
     if (marge <= 8) {
-        return ["Dommage : Chute K 2"];
+        return [["Dommage","Chute K 2"]];
     }
     if (marge <= 9) {
-        return ["Dommage : Bls : K 3"];
+        return [["Dommage","Bls : K 3"]];
     }
     if (marge <= 10) {
-        return ["Dommage : Bls : K 4"];
+        return [["Dommage","Bls : K 4"]];
     }
     if (marge <= 11) {
-        return ["Dommage : Bls O 4"];
+        return [["Dommage","Bls O 4"]];
     }
     if (marge <= 12) {
-        return ["Dommage : Bls K 5"];
+        return [["Dommage","Bls K 5"]];
     }
     if (marge <= 13) {
-        return ["Dommage : 2 Bls O 4"];
+        return [["Dommage","2 Bls O 4"]];
     }
     if (marge <= 14) {
-        return ["Dommage : 2 Bls KO 5"];
+        return [["Dommage","2 Bls KO 5"]];
     }
     if (marge <= 15) {
-        return ["Dommage : 3 Bls KO 5"];
+        return [["Dommage","3 Bls KO 5"]];
     }
     let nbBls = 3 + Math.floor(((marge - 16)/2));
-    return["Dommage : "+nbBls+" Bls KO 5"];
+    return[["Dommage",nbBls+" Bls KO 5"]];
 }
 
 function margeToDamageForNormCharacter(marge) {
     if (marge <= 0) {
-        return ["Aucun dommage"];
+        return [["!","Aucun dommage"]];
     }
     if (marge <= 1) {
-        return ["Dommage : 1"];
+        return [["Dommage","1"]];
     }
     if (marge <= 2) {
-        return ["Dommage : O 1"];
+        return [["Dommage","O 1"]];
     }
     if (marge <= 3) {
-        return ["Dommage : K 1"];
+        return [["Dommage","K 1"]];
     }
     if (marge <= 4) {
-        return ["Dommage : 2"];
+        return [["Dommage","2"]];
     }
     if (marge <= 5) {
-        return ["Dommage : O 2"];
+        return [["Dommage","O 2"]];
     }
     if (marge <= 6) {
-        return ["Dommage : Chute O 2"];
+        return [["Dommage","Chute O 2"]];
     }
     if (marge <= 8) {
-        return ["Dommage : Chute K 2"];
+        return [["Dommage","Chute K 2"]];
     }
     if (marge <= 9) {
-        return ["Dommage : Bls : K 3"];
+        return [["Dommage","Bls : K 3"]];
     }
     if (marge <= 10) {
-        return ["Dommage : Bls : K 4"];
+        return [["Dommage","Bls : K 4"]];
     }
     if (marge <= 11) {
-        return ["Dommage : Bls O 4"];
+        return [["Dommage","Bls O 4"]];
     }
     if (marge <= 12) {
-        return ["Dommage : Bls K 5"];
+        return [["Dommage","Bls K 5"]];
     }
     if (marge <= 13) {
-        return ["Dommage : 2 Bls O 4"];
+        return [["Dommage","2 Bls O 4"]];
     }
     if (marge <= 14) {
-        return ["Dommage : 2 Bls KO 5"];
+        return [["Dommage","2 Bls KO 5"]];
     }
     if (marge <= 15) {
-        return ["Dommage : 3 Bls KO 5"];
+        return [["Dommage","3 Bls KO 5"]];
     }
     let nbBls = 3 + Math.floor(((marge - 16)/2));
-    return["Dommage : "+nbBls+" Bls KO 5"];
+    return[["Dommage",nbBls+" Bls KO 5"]];
 }
 
 function sendResultToChat(who, result) {
+
+    const torgColor="#e7f106";
+    
+    const colorLine1="#b6ab91";
+    const colorLine2="#cec7b6";
+    
+    const divHeaderStyle = "<div style='font-family: Impact; font-size: 1.2em; line-height: 1.2em; font-weight: normal; font-style: normal; font-variant: normal; letter-spacing: 2px; text-align: center; vertical-align: middle; margin: 0px; padding: 2px 0px 0px 0px; border: 1px solid #000000; border-radius: 5px 5px 0px 0px; color: #000000; text-shadow: -1px -1px 0 #ffffff , 1px -1px 0 #ffffff , -1px 1px 0 #ffffff , 1px 1px 0 #ffffff; background-color: "+ torgColor +"; background-image: linear-gradient( rgba( 255 , 255 , 255 , .3 ) , rgba( 255 , 255 , 255 , 0 ) )';>";
+    const spanSubTitleStyle = "<span style='font-family: tahoma; font-size: 13px; font-weight: normal; font-style: normal; font-variant: normal; letter-spacing: 1px';>";
+    const divLineStyle = "<div style='color: #000000; background-color: |color|; line-height: 1.1em; vertical-align: middle; font-family: helvetica; font-size: 14px; font-weight: normal; font-style: normal; text-align: left; padding: 4px 5px 2px 5px; border-left: 1px solid #000000; border-right: 1px solid #000000;'>";
+    const divLastLineStyle = "<div style='color: #000000; background-color: |color|; line-height: 1.1em; vertical-align: middle; font-family: helvetica; font-size: 14px; font-weight: normal; font-style: normal; text-align: left; padding: 4px 5px 2px 5px; border-left: 1px solid #000000; border-right: 1px solid #000000; border-bottom: 1px solid #000000; border-radius: 0px 0px 5px 5px;'>";
+    Content = divHeaderStyle + result.title + "<br/>";
+    
+    if (result.subtitle !== undefined)
+        Content += spanSubTitleStyle + result.subtitle + "</span>";
+    
+    Content += "</div>";
+    
+    for  (var i = 0; i < result.infoList.length; i++) { 
+    
+    
+        if (i==result.infoList.length-1)
+            lineStyle = (i % 2 ==1 || result.infoList.length==1 ) ? divLastLineStyle.replace("|color|",colorLine1) : divLineStyle.replace("|color|",colorLine2);
+        else
+            lineStyle = (i % 2 ==1) ? divLineStyle.replace("|color|",colorLine1) : divLineStyle.replace("|color|",colorLine2);
+        
+
+        if (result.infoList[i][0].startsWith("!"))
+            Content += lineStyle + result.infoList[i][1] + "</div>"
+        else
+            Content += lineStyle + "<strong>" + result.infoList[i][0] + "</strong> " + result.infoList[i][1] +  "</div>"
+        
+        
+    }
+
+    sendChat("",who + " " + Content); 
+
+
+/*
     //TODO Fred
     sendChat(who, result.title);
     if (result.infoList != undefined) {
         result.infoList.forEach(info => sendChat(who, info));
     }
+*/
 }
